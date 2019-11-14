b2ddee8c9998e62f04ebae55b0b897c7c0d47873
Amaury <amauryverschooren@icloud.com>
2019-11-14 09:35:17 +0100

    e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -4,9 +4,9 @@ import { browser, logging } from 'protractor';
 describe('workspace-project App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+  // beforeEach(() => {
+  //   page = new AppPage();
+  // });
 
   // it('should display welcome message', () => {
   //   page.navigateTo();
