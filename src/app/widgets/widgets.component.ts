bb4ecbb29e992634f0277ad6b464e13cc8d68c74
Amaury <amauryverschooren@icloud.com>
2019-11-19 20:33:01 +0100

    files

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-widgets',
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class WidgetsComponent implements OnInit {
 
   constructor() { }
-
+ 
   ngOnInit() {
+    
   }
-  ChangeColor() { }
-  checkValue(event: any) {
-    console.log(event);
- }}
+  
+
+  }
  
