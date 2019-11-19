60914e6a4372d114e13a19aa73e8f0453c9d8b34
Jordy De Groeve <jordy.dg@hotmail.com>
2019-11-20 00:35:35 +0100

    Toggle between darkmode and default colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'AngularWebsite';
+
+  toggleDarkMode(event: any) {
+    if (event.target.checked) {
+      document.getElementsByTagName('body').item(0).setAttribute('data-theme', 'dark');
+    } else {
+      document.getElementsByTagName('body').item(0).removeAttribute('data-theme');
+    }
+  }
 }
