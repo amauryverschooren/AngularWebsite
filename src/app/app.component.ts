b3e61dfeab5a4742c98d6488582e2420e9da0b1c
Amaury <amauryverschooren@icloud.com>
2019-11-20 12:01:07 +0100

    Added Calendar & Added Weather Forecast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,20 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title = 'AngularWebsite';
 
+  Datum : Date;
+
   toggleDarkMode(event: any) {
     if (event.target.checked) {
       document.getElementsByTagName('body').item(0).setAttribute('data-theme', 'dark');
     } else {
       document.getElementsByTagName('body').item(0).removeAttribute('data-theme');
     }
+
+
+    
+  }
+
+  constructor(){
+    this.Datum = new Date();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {WidgetsComponent} from './widgets/widgets.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WidgetsComponent
+    AppComponent
   ],
   exports: [
-    AppComponent,
-    WidgetsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
